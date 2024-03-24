0975520bd33c4b435772c6178d896a13f1192706
Kanapiya <n1ght.d1ck1ns@gmail.com>
2024-03-24 11:51:31 +0500

    Add new screen and debug functions

--- lab5/SecondViewController.swift
+++ lab5/SecondViewController.swift
@@ -7,15 +7,27 @@
 
 import UIKit
 
+protocol SecondViewControllerDelegate: AnyObject {
+    func updateReceivedMessage(_ message: String?)
+}
+
 class SecondViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
     
+    // Переменная для хранения полученного сообщения
     var receivedMessage: String?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // При загрузке экрана устанавливаем текст сообщения
-        label.text = receivedMessage
+    // Делегат для обновления данных в ViewController
+    weak var delegate: SecondViewControllerDelegate?
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Обновляем значение в label при каждом появлении экрана
+        label.text = receivedMessage ?? "Нет данных"
+        
+        // Вызываем метод делегата для передачи данных обратно в ViewController
+        delegate?.updateReceivedMessage(receivedMessage)
     }
 }
--- lab5/ViewController.swift
+++ lab5/ViewController.swift
@@ -11,23 +11,36 @@ class ViewController: UIViewController {
     @IBOutlet var buttonNext: UIButton!
     @IBOutlet weak var textField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // Статическая переменная для хранения введенного текста
+    static var enteredText: String?
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToSecondScreenSegue" {
+            if let destinationVC = segue.destination as? SecondViewController {
+                print("Передаваемый текст:", textField.text ?? "No text provided")
+                destinationVC.receivedMessage = textField.text
+            }
+        }
     }
     
     @IBAction func goToTheSecondScreen (_ sender: UIButton) {
+        guard let text = textField.text, !text.isEmpty else {
+            print("Введенный текст пуст")
+            return
+        }
+
+        // Сохраняем текст в статической переменной для передачи на следующий экран
+        ViewController.enteredText = text
+        print("Entered text: \(text)")
+
         performSegue(withIdentifier: "goToSecondScreenSegue", sender: self)
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           if segue.identifier == "goToSecondScreenSegue" {
-               if let destinationVC = segue.destination as? SecondViewController {
-                   // Передаем текстовое сообщение на SecondViewController
-                   destinationVC.receivedMessage = textField.text
-               }
-           }
-       }
-    
-    
+}
+
+extension ViewController: SecondViewControllerDelegate {
+    // Реализация метода протокола для обновления данных в SecondViewController
+    func updateReceivedMessage(_ message: String?) {
+        // Обновляем значение enteredText при обновлении данных в SecondViewController
+        ViewController.enteredText = message
+    }
 }
